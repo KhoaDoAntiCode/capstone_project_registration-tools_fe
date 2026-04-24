159eedf8b7e1bd62eb179fccbcbf52ab372c0b2c
KhoaDoAntiCode <khoado0123@gmail.com>
2026-04-24 15:53:09 +0700

    feat: add generated API client

---
+++ src/lib/api/generated/index.ts
@@ -0,0 +1,19 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export { ApiError } from './core/ApiError';
+export { CancelablePromise, CancelError } from './core/CancelablePromise';
+export { OpenAPI } from './core/OpenAPI';
+export type { OpenAPIConfig } from './core/OpenAPI';
+
+export type { LoginRequest } from './models/LoginRequest';
+export type { RegisterRequest } from './models/RegisterRequest';
+export type { ReviewRequest } from './models/ReviewRequest';
+export type { StudentRequest } from './models/StudentRequest';
+export type { SubmitProjectRequest } from './models/SubmitProjectRequest';
+export type { SupervisorRequest } from './models/SupervisorRequest';
+
+export { AuthService } from './services/AuthService';
+export { ProjectService } from './services/ProjectService';
+export { ReviewService } from './services/ReviewService';
---
+++ src/lib/api/generated/services/AuthService.ts
@@ -0,0 +1,41 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { LoginRequest } from '../models/LoginRequest';
+import type { RegisterRequest } from '../models/RegisterRequest';
+import type { CancelablePromise } from '../core/CancelablePromise';
+import { OpenAPI } from '../core/OpenAPI';
+import { request as __request } from '../core/request';
+export class AuthService {
+    /**
+     * @param requestBody
+     * @returns any OK
+     * @throws ApiError
+     */
+    public static postApiAuthRegister(
+        requestBody?: RegisterRequest,
+    ): CancelablePromise<any> {
+        return __request(OpenAPI, {
+            method: 'POST',
+            url: '/api/auth/register',
+            body: requestBody,
+            mediaType: 'application/json',
+        });
+    }
+    /**
+     * @param requestBody
+     * @returns any OK
+     * @throws ApiError
+     */
+    public static postApiAuthLogin(
+        requestBody?: LoginRequest,
+    ): CancelablePromise<any> {
+        return __request(OpenAPI, {
+            method: 'POST',
+            url: '/api/auth/login',
+            body: requestBody,
+            mediaType: 'application/json',
+        });
+    }
+}
